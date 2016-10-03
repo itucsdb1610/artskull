df14e9b1d431cc24d45ea85002f33552094fd0f9
Muhammed Kadir Yücel <Muhammed Kadir Ycel>
2016-10-03 10:46:14 +0300

    operations for bluemix

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask import render_template
+import os
 
 app = Flask(__name__)
+
+# For Bluemix
+port = int(os.getenv('VCAP_APP_PORT', 8080))
 
 @app.route('/')
 def home():
@@ -9,4 +13,4 @@ def home():
 
 if __name__ == '__main__':
     #next line should change on deployment!
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=port)
