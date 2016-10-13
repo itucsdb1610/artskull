2a0e94a4cb1b4d075c42720c0dc9ff9930baa75c
Muhammed Kadir Yücel <mky515@gmail.com>
2016-10-13 21:02:57 +0300

    app will run in localhost for test purposes

--- server.py
+++ server.py
@@ -12,5 +12,5 @@ def home():
     return render_template('home.html')
 
 if __name__ == '__main__':
-    #next line should change on deployment!
-    app.run(host='0.0.0.0', port=port)
+    #app.run(host='0.0.0.0', port=port) #for bluemix
+    app.run(host='localhost', port='5000') #comment this while publishing to bluemix
