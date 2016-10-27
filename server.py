47b8887b3a7f2f9aecb5ff7912aa570891b7d5b8
Muhammed Kadir Yücel <mky515@gmail.com>
2016-10-27 13:41:08 +0300

    Settings moved out and Blueprint support added

--- server.py
+++ server.py
@@ -1,41 +1,27 @@
-from flask import Flask
-from flask import render_template, session, redirect
+from flask import Flask, render_template
 import os
+from handlers import site
 
-app = Flask(__name__)
-app.secret_key = "itucsdb1610"
 # For Bluemix
 port = int(os.getenv('VCAP_APP_PORT', 8080))
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('settings')
+    app.secret_key = "itucsdb1610"
+    app.register_blueprint(site)
 
-@app.route('/timeline')
-def timeline():
-    return render_template('timeline.html')
+    return app
 
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
 
-@app.route('/content')
-def content():
-    return render_template('content.html')
+def main():
+    app = create_app()
+    debug = app.config['DEBUG']
+    port = app.config['PORT']
+    #app.run(host='0.0.0.0', port=port) #for bluemix
+    app.run(host='localhost', port=port, debug=debug) #comment this while publishing to bluemix
 
-@app.route('/admin')
-def admin():
-    return render_template('contentadmin.html')
-
-@app.route('/search')
-def search():
-    return render_template('searchresult.html')
-
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    session['logged_in'] = True
-    return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    #app.run(host='0.0.0.0', port=port) #for bluemix
-    app.run(host='localhost', port='5000') #comment this while publishing to bluemix
+    main()
+    
