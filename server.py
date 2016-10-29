2a21c716f4bf31ff0a1c140230f36650ddf496bd
Muhammed Kadir Yücel <mky515@gmail.com>
2016-10-29 16:02:53 +0300

    Fix for match variable

--- server.py
+++ server.py
@@ -83,7 +83,7 @@ if __name__ == '__main__':
 
     VCAP_SERVICES = os.getenv('VCAP_SERVICES')
     if VCAP_SERVICES is not None:
-        app.config['dsn'] = get_elephant_dsn(VCAP_SERVICES)
+        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
     else:
         app.config['dsn'] = """user='vagrant' password='vagrant' host='localhost' port=54321 dbname='itucsdb1610'"""
     app.run(host='0.0.0.0', port=port, debug=debug)
