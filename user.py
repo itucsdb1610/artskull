da3d5e62b6b36479303fe012ea837975fd93937c
Muhammed Kadir Yücel <mky515@gmail.com>
2016-10-29 18:57:32 +0300

    Class definiton for users

---
+++ user.py
@@ -0,0 +1,9 @@
+class User:
+    def __init__(self, username, passwd, email, name, surname, genres, profpic):
+        self.username = username
+        self.passwd = passwd
+        self.email = email
+        self.name = name
+        self.surname = surname
+        self.genres = genres
+        self.profpic = profpic
