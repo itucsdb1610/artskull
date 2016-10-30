bbbb2ed607f18c53cc472b4c194f2af89edb3393
ozcelikfu <ozcelikfu@itu.edu.tr>
2016-10-30 17:29:03 +0200

    Content class for database added
    It has 7 attributes id, title, artist, duration, date, contentpic

---
+++ content.py
@@ -0,0 +1,8 @@
+class Content:
+    def __init__(self, title, artist, duration, date, genres, contentpic):
+        self.title = title
+        self.artist = artist
+        self.duration = duration
+        self.date = date
+        self.contentpic = contentpic
+        self.genres = genres
