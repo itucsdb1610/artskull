a1d6ace82c7c8faffbd685b489b45e71349d66f5
ozcelikfu <ozcelikfu@itu.edu.tr>
2016-11-29 09:29:41 +0300

    Stage object is added

---
+++ stage.py
@@ -0,0 +1,6 @@
+class Stage:
+    def __init__(self, name, location, capacity, stagepic):
+        self.name = name
+        self.location = location
+        self.capacity = capacity
+        self.stagepic = stagepic
