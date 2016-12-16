e9da49b0b71bdacec71d5e07c446e29b256ed94a
ozkokmur <ozkokmur@itu.edu.tr>
2016-12-16 03:58:15 +0300

    Notification class is added

---
+++ notification.py
@@ -0,0 +1,6 @@
+class Notification:
+    def __init__(self, commentid,commenter, receiver, date):
+        self.commentid = commentid
+        self.commenter = commenter
+        self.receiver = receiver
+        self.date = date
